115b8aacfd4c848102d5b38a9f6e22c1ca16641a
slavc <v.inderiakin.uk@gmail.com>
2020-11-12 20:18:09 -0800

    added default buttons

--- app.py
+++ app.py
@@ -9,23 +9,23 @@
 # $ set FLASK_ENV=development                   (only during debug)
 # $ flask run
 # after which go to the link displayed in the terminal window
-
+#
+# code sources using partly in this project
+# https://www.fullstackpython.com/blog/responsive-bar-charts-bokeh-flask-python-3.html
 
 from flask import Flask, render_template
 import random
 
-from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,
-                          Range1d)
-from bokeh.models.glyphs import VBar
+from bokeh.models import HoverTool
 from bokeh.plotting import figure
-# from bokeh.plotting import Bar
 from bokeh.embed import components
-from bokeh.models.sources import ColumnDataSource
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def chart():
+def main_window():
     bars_count = 6
     if bars_count <= 0:
         bars_count = 1
@@ -37,19 +37,30 @@ def chart():
         data['costs'].append(random.uniform(1.00, 1000.00))
 
     hover = create_hover_tool()
-    plot = create_bar_chart()
+    plot = create_bar_chart(hover)
     script, div = components(plot)
 
-    return render_template("chart.html", bars_count=bars_count,
+    return render_template("home.html",
                            the_div=div, the_script=script)
 
 
 def create_hover_tool():
-    # we'll code this function in a moment
-    return None
+    """Generates the HTML for the Bokeh's hover data tool on our graph."""
+    hover_html = """
+      <div>
+        <span class="hover-tooltip">$x</span>
+      </div>
+      <div>
+        <span class="hover-tooltip">@bugs bugs</span>
+      </div>
+      <div>
+        <span class="hover-tooltip">$@costs{0.00}</span>
+      </div>
+    """
+    return HoverTool(tooltips=hover_html)
 
 
-def create_bar_chart():
+def create_bar_chart(hover_tool=None):
     """Creates a bar chart plot with the exact styling for the centcom
        dashboard. Pass in data as a dictionary, desired plot title,
        name of x axis, y axis and the hover tool HTML.
@@ -59,8 +70,11 @@ def create_bar_chart():
     x = [1, 2, 3, 4, 5]
     y = [6, 7, 2, 4, 5]
 
+    if hover_tool:
+        tools = [hover_tool,]
+
     # create a new plot with a title and axis labels
-    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y', tools=tools)
 
     # add a line renderer with legend and line thickness
     p.line(x, y, legend_label="Temp.", line_width=2)
