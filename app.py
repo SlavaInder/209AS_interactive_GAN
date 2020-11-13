f51cfce6353dc2e9adff945d476fd5024f2f93c5
slavc <v.inderiakin.uk@gmail.com>
2020-11-12 18:05:13 -0800

    added default plot

--- app.py
+++ app.py
@@ -11,9 +11,61 @@
 # after which go to the link displayed in the terminal window
 
 
-from flask import Flask
+from flask import Flask, render_template
+import random
+
+from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,
+                          Range1d)
+from bokeh.models.glyphs import VBar
+from bokeh.plotting import figure
+# from bokeh.plotting import Bar
+from bokeh.embed import components
+from bokeh.models.sources import ColumnDataSource
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def chart():
+    bars_count = 6
+    if bars_count <= 0:
+        bars_count = 1
+
+    data = {"days": [], "bugs": [], "costs": []}
+    for i in range(1, bars_count + 1):
+        data['days'].append(i)
+        data['bugs'].append(random.randint(1,100))
+        data['costs'].append(random.uniform(1.00, 1000.00))
+
+    hover = create_hover_tool()
+    plot = create_bar_chart()
+    script, div = components(plot)
+
+    return render_template("chart.html", bars_count=bars_count,
+                           the_div=div, the_script=script)
+
+
+def create_hover_tool():
+    # we'll code this function in a moment
+    return None
+
+
+def create_bar_chart():
+    """Creates a bar chart plot with the exact styling for the centcom
+       dashboard. Pass in data as a dictionary, desired plot title,
+       name of x axis, y axis and the hover tool HTML.
+    """
+
+    # prepare some data
+    x = [1, 2, 3, 4, 5]
+    y = [6, 7, 2, 4, 5]
+
+    # create a new plot with a title and axis labels
+    p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
+    # add a line renderer with legend and line thickness
+    p.line(x, y, legend_label="Temp.", line_width=2)
+    return p
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
