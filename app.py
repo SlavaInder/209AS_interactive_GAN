9c1f6df419a8bbbc7d9607d6c394f4eb673ee6a2
slavc <v.inderiakin.uk@gmail.com>
2020-11-14 17:54:03 -0800

    added generator

--- app.py
+++ app.py
@@ -16,9 +16,10 @@
 import igan
 
 from flask import Flask, render_template, request
-import random
 
-from bokeh.models import HoverTool
+from bokeh.models import PointDrawTool, ColumnDataSource, BoxSelectTool
+from bokeh.models.widgets import RadioButtonGroup
+from bokeh.models.callbacks import CustomJS
 from bokeh.plotting import figure
 from bokeh.embed import components
 
@@ -27,68 +28,88 @@ from bokeh.embed import components
 app = Flask(__name__)
 app.secret_key = 'some secret key'
 
-# the array with the original data
-original_array = None
 
 # init handlers
-load_handler = igan.LoadDataFormHandler(original_array, "load_file")
+load_handler = igan.LoadDataFormHandler("load_data_button", "input_file")
+generator_handler = igan.GenerateDataFormHandler("generate_data_button")
 
 # init handler manager
-manager = igan.HandlerManager([load_handler])
+manager = igan.HandlerManager([load_handler,
+                               generator_handler])
+
+# init data dictionary
+data = {'orig_x': [], 'orig_y': [], 'gen_x': [], 'gen_y': [], }
+# init dictionary for UI elements
+ui = {"original_select": "select-button",
+      "synthesized_select": "unselect-button"}
 
 
 @app.route('/', methods=['GET', 'POST', 'DELETE'])
 def main_window():
 
     if request.method == 'POST':
-        print(request.form)
-        manager.handle(request)
+        # prepare data pack for messages
+        data_pack = {'data_dict': data}
+        # get updates from the manager
+        updates = manager.handle(request, data_pack)
+        # manage updates
+        if "orig_data_vals" in updates:
+            # update dict
+            data['orig_y'] = updates["orig_data_vals"]
+        if "orig_data_timestamps" in updates:
+            data['orig_x'] = updates["orig_data_timestamps"]
+        if "gen_data_vals" in updates:
+            # update dict
+            data['gen_x'] = updates["gen_data_vals"]
+        if "gen_data_timestamps" in updates:
+            data['gen_y'] = updates["gen_data_timestamps"]
+        if "change_to_orig" in updates:
+            ui["original_select"] = "select-button"
+            ui["synthesized_select"] = "unselect-button"
+        if "change_to_gen" in updates:
+            ui["original_select"] = "unselect-button"
+            ui["synthesized_select"] = "select-button"
 
-    hover = create_hover_tool()
-    plot = create_bar_chart(hover)
+    a = request.form.get("plot_source")
+    print(a)
+
+    plot = create_chart()
     script, div = components(plot)
 
     return render_template("home.html",
                            the_div=div,
-                           the_script=script)
+                           the_script=script,
+                           UI=ui)
 
 
-def create_hover_tool():
-    """Generates the HTML for the Bokeh's hover data tool on our graph."""
-    hover_html = """
-      <div>
-        <span class="hover-tooltip">$x</span>
-      </div>
-      <div>
-        <span class="hover-tooltip">@bugs bugs</span>
-      </div>
-      <div>
-        <span class="hover-tooltip">$@costs{0.00}</span>
-      </div>
-    """
-    return HoverTool(tooltips=hover_html)
+def create_chart():
+    # divide dict into parts
+    orig_data = {"orig_x": data["orig_x"],
+                 "orig_y": data["orig_y"]}
+    gen_data = {"gen_x": data["gen_x"],
+                "gen_y": data["gen_y"]}
 
+    # create a corresponding data source objects
+    source = ColumnDataSource(data=orig_data)
+    added_points_source = ColumnDataSource(data={'orig_x': [], 'orig_y': []})
 
-def create_bar_chart(hover_tool=None):
-    """Creates a bar chart plot with the exact styling for the centcom
-       dashboard. Pass in data as a dictionary, desired plot title,
-       name of x axis, y axis and the hover tool HTML.
-    """
+    # init necessary tools
+    tools = ["pan,wheel_zoom,box_zoom,reset"]
 
-    # prepare some data
-    x = [1, 2, 3, 4, 5]
-    y = [6, 7, 2, 4, 5]
-
-    if hover_tool:
-        tools = [hover_tool,]
+    radio_button_group = RadioButtonGroup(
+        labels=["Option 1", "Option 2", "Option 3"], active=0)
 
     # create a new plot with a title and axis labels
     p = figure(title="simple line example", x_axis_label='x', y_axis_label='y', tools=tools)
-
     # add a line renderer with legend and line thickness
-    p.line(x, y, legend_label="Temp.", line_width=2)
+    p.line(x='orig_x', y='orig_y', source=source, legend_label="Temp.", line_width=2)
+    # add a circle renderer with a size, color, and alpha
+    p.circle(x='orig_x', y='orig_y', source=source, size=3, color="navy", alpha=0.5)
+
+    # create a rendering tool for additional points
+    r1 = p.circle('orig_x', 'orig_y', size=3, color="red", source=added_points_source)
+    draw_tool = PointDrawTool(renderers=[r1])
+    p.add_tools(draw_tool)
+    p.add_tools(BoxSelectTool(dimensions="width"))
+
     return p
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- igan/request_handlers.py
+++ igan/request_handlers.py
@@ -1,12 +1,27 @@
-from flask import flash
+import scipy.io
+import numpy as np
+
 
 # some constants we will need
-ALLOWED_EXTENSIONS = {'.mat', '.csv'}
+ALLOWED_EXTENSIONS = ['mat', 'csv']
 
 
+# dummy function to emulate data generation
+def dummy_generator(values, timestamps):
+    return values, timestamps
+
+
+# a function to check whether the file format is allowed
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# converts a dictionary with a single entry "val" to 2 arrays: time stamp and value
+def ecg_mat_to_np_converter(inp):
+    values = inp["val"].flatten()
+    timestamps = np.arange(len(values))
+    return values, timestamps
 
 
 # class that manages of all handlers
@@ -14,41 +29,79 @@ class HandlerManager(object):
     def __init__(self, handlers):
         self.handlers = handlers
 
-    def handle(self, form):
+    def handle(self, form, data_pack):
+        updates = {}
         for handler in self.handlers:
-            handler.handle(form)
+            updates.update(handler.handle(form, data_pack))
+
+        return updates
 
 
 # a base class for all interactions with forms
 class FormHandler(object):
     # an interface that all interactions should support
-    def handle(self, request):
+    def handle(self, request, data_pack):
         updates = []
         return updates
 
 
-# class to handle data loading
-class LoadDataFormHandler(FormHandler):
-    # original_data - a reference to the object where original data is stored
+# class to handle data generation
+class GenerateDataFormHandler(FormHandler):
     # button_name - name of the button used for form submission
-    def __init__(self, original_data, button_name):
-        # save pointer to the array with the original data
-        self.original_data = original_data
+    def __init__(self, button_name):
         # save the name of the form this handler is connected to
         self.button_name = button_name
 
-    # this function loads data in the system and updates the chart
-    def handle(self, request):
+    # generate data using GAN
+    def handle(self, request, data_pack):
         # handle only in case load button is pressed and there is a button
-        if "load_data_button" in request.form:
-            if 'file' not in request.files:
-                print("hey!")
-                flash("no file is selected")
+        if self.button_name in request.form:
+            # if necessary data is loaded into the pack
+            if 'data_dict' in data_pack:
+                # replace dummy generator later
+                values, timestamps = dummy_generator(data_pack['data_dict']['orig_y'], data_pack['data_dict']['orig_x'])
+                updates = {"gen_data_vals": values,
+                           "gen_data_timestamps": timestamps,
+                           "change_to_gen": True}
+                return updates
+            else:
+                return {}
+        else:
+            return {}
+
+
+# class to handle data loading
+class LoadDataFormHandler(FormHandler):
+    # button_name - name of the button used for form submission
+    def __init__(self, button_name, input_field_name):
+        # save the name of the form this handler is connected to
+        self.button_name = button_name
+        # save the name of the form this handler is connected to
+        self.input_field_name = input_field_name
+
+    # this function loads data in the system and updates the chart
+    def handle(self, request, data_pack):
+        # handle only in case load button is pressed and there is a button
+        if self.button_name in request.form:
+            # we don't have to check this - it just confirms that there is a field for input file
+            if self.input_field_name not in request.files:
+                return {}
             else:
                 # get file from the request object
-                file = request.files['file']
+                file = request.files[self.input_field_name]
                 # if no file is submitted, do nothing
                 if file.filename == '':
-                    flash('no file is selected')
+                    return {}
+                # if file extension is allowed
                 elif file and allowed_file(file.filename):
-                    print("good file!")
+                    if file.filename.rsplit('.', 1)[1].lower() == "mat":
+                        original_data = scipy.io.loadmat(file.stream)
+                        updates = {"orig_data_vals": (ecg_mat_to_np_converter(original_data))[0],
+                                   "orig_data_timestamps": (ecg_mat_to_np_converter(original_data))[1]}
+                        return updates
+                    else:
+                        return {}
+                else:
+                    return {}
+        else:
+            return {}
